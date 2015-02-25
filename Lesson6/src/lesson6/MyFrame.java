51c9957c0e64932f378d2546901c1e31c88d6843
paralainer <serg.talov@gmail.com>
2015-02-25 11:10:46 +0300

    Lesson6

---
+++ Lesson6/src/lesson6/MyFrame.java
@@ -0,0 +1,14 @@
+package lesson6;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	public MyFrame(){
+		getContentPane().add(new MyImgPanel());
+		setBounds(0, 0, 1000, 1000);
+		setVisible(true);
+	}
+}
